fc8d0704eb2cb38638712b19c24fb4e564f4a431
gitpetr <pkhilenko@yandex.ru>
2017-06-20 18:49:39 +0600

    tests for PersonManager: get_person, set_person, remove_person

--- test/models/person_manager_test.rb
+++ test/models/person_manager_test.rb
@@ -3,32 +3,37 @@ require 'test_helper'
 class PersonManagerTest < ActiveSupport::TestCase
 
   def setup
-    session = {}
-    @person_manager = PersonManager.new session
-    @user = PersonManager.create_person('vasya@mail.ru')
-    @person_manager.add_person(@user)
-    @person_manager.save_users
-    @user2 = PersonManager.create_person('lena@rambler.ru')
-    @person_manager.add_person(@user2)
-    @person_manager.save_users
-    @user3 = PersonManager.create_person('yula@ya.ru')
-    @person_manager.add_person(@user3)
-    @person_manager.save_users
+    @person_manager = PersonManager.new(PersonSessionStore.new({}))
+    @person_factory = PersonFactory.new(@person_manager.max_id)
 
+    @person = @person_factory.create_person('vasya@mail.ru')
+    @person_manager.set_person @person
+    @person_manager.save_persons
+
+    @person2 = @person_factory.create_person('gena@mail.ru')
+    @person_manager.set_person @person2
+    @person_manager.save_persons
+
+    @person3 = @person_factory.create_person('genadiy@mail.ru')
+    @person_manager.set_person @person3
+    @person_manager.save_persons
   end
 
-  test 'create_person' do
-    assert_equal('vasya@mail.ru', @person_manager.users[1].user_name)
+  test 'get_person' do
+    assert_equal(@person_manager.get_person(1).name, 'vasya@mail.ru')
   end
 
-  test 'edit person' do
-    @person_manager.person_edit(2, 'gena@mail.ru')
-    assert_equal('gena@mail.ru', @person_manager.users[2].user_name )
+  test 'set_person' do
+    @person2.name = 'lena@ya.ru'
+    @person_manager.set_person(@person2)
+    @person_manager.save_persons
+    assert_equal(@person_manager.get_person(2).name, 'lena@ya.ru')
   end
 
-  test 'delete person' do
-    @person_manager.person_delete(3)
-    assert_equal(nil, @person_manager.users[3])
+  test 'remove_person' do
+    @person_manager.remove_person(@person3)
+    @person_manager.save_persons
+    assert_equal(@person_manager.get_person(3), nil)
   end
 
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class PersonTest < ActiveSupport::TestCase
   test 'get @person.to_h' do
     @person = Person.new(1, 'vasya@mail.ru')
-    assert_equal({ id: 1, user_name: 'vasya@mail.ru' }, @person.to_h)
+    assert_equal({ id: 1, name: 'vasya@mail.ru' }, @person.to_h)
   end
 
   test 'get obj.user_name' do
     @person = Person.new(1, 'vasya@mail.ru')
-    assert_equal('vasya@mail.ru', @person.user_name)
+    assert_equal('vasya@mail.ru', @person.name)
   end
 
   test 'get obj.id' do
