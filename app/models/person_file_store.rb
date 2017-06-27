8b51e37beaf23b6ed39b3a728b1745b49b845af9
gitpetr <pkhilenko@yandex.ru>
2017-06-27 18:28:45 +0600

    rename txt to json

--- app/models/person_file_store.rb
+++ app/models/person_file_store.rb
@@ -2,16 +2,16 @@ class PersonFileStore
   require 'json'
 
   def initialize
-    @textfile = File.expand_path('../person_store.txt', __FILE__)
+    @person_store = File.expand_path('../person_store.json', __FILE__)
   end
 
   def read
-    persons = JSON.parse(File.open(@textfile, 'r'){ |file| file.read }) || {}
+    persons = JSON.parse(File.open(@person_store, 'r'){ |file| file.read }) || {}
     persons.keys.inject({}) { |h, id| h[id.to_i] = Person.new(id.to_i,  persons[id]); h }
   end
 
   def write(persons)
     persons = persons.values.inject({}) { |h, person| h[person.id] = person.to_h; h }
-    File.open(@textfile, 'w'){ |file| file.puts JSON.generate(persons) }
+    File.open(@person_store, 'w'){ |file| file.puts JSON.generate(persons) }
   end
 end
